6c35664f1dc875d4a327f10bf0ddd38de34f15e6
jaredbnm <jaredjr0204@gmail.com>
2025-03-31 12:29:58 -0500

    finished requirements 1 and 2

---
+++ jbenman-lab4.js
@@ -0,0 +1,19 @@
+// J Benman
+// ITMD 441-01 Undergrad Student
+
+// requirement 1: change hero headline
+(function() {
+  document.querySelector('#hero h1').textContent = "Uplift Your Brand with Stellar Marketing";
+})();
+
+// requirement 2: change the line of text below the hero headline
+(function() {
+  document.querySelector('#hero p').innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business <i><b>thrive</b></i> and <i><b>excel</b></i>.";
+})();
+
+// requirement 3:
+// (function() {
+//   document.body.style.backgroundImage = "url('https://picsum.photos/id/683/1280/720')";
+// })();
+
+// requirement 4: 
