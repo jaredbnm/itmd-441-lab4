e14472120a48d5c6f65e77fc84ef62301d18392e
jaredbnm <jaredjr0204@gmail.com>
2025-03-31 13:20:25 -0500

    added more requirements

--- jbenman-lab4.js
+++ jbenman-lab4.js
@@ -11,9 +11,33 @@
   document.querySelector('#hero p').innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business <i><b>thrive</b></i> and <i><b>excel</b></i>.";
 })();
 
-// requirement 3:
+// requirement 3: change the image in the background of the hero
 // (function() {
 //   document.body.style.backgroundImage = "url('https://picsum.photos/id/683/1280/720')";
 // })();
 
-// requirement 4: 
+// requirement 4: change the background color of the nav bar
+(function() {
+  document.querySelector('header').style.backgroundColor = 'rgb(31 41 55 / var(--tw-bg-opacity, 1))';
+})();
+
+// requirement 5: remove the “Get Started” CTA 
+(function() {
+  document.querySelector('#hero a').remove();
+})();
+
+// requirement 6: center align the heading for sections
+(function() {
+  document.querySelector('nav').style.textAlign = 'left';
+})();
+
+// requirement 7: change services icons color
+(function() {
+  const icons = document.querySelector('#services span');
+  if (icons) {
+    icons.style.color = '#47C714';
+  }
+})();
+
+// requirement 8: change the digital marketing icon to use 'Ads Click'
+
